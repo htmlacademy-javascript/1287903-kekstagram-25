ffbbe7533ba35fed6d1b265c2a8f2bd148c78255
sarvanshiriev <sarvan.shiriev@mail.ru>
2022-02-25 11:25:28 +0500

    Изменения от 25.02.2022

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-function number(min,max) {
+const MAX_VALUE= 10;
+
+function returnNumber(min,max) {
   return Math.floor((Math.random()*(max-min+1))+min);
 }
-number(1,10);
+returnNumber(1,10);
 
-const MAX_VALUE= 10;
 function checkLength(comment,max) {
   return comment.length<=max;
 }
-checkLength("Привет всем,как дела?Как жизнь?!",MAX_VALUE);
+checkLength('Привет всем,как дела?Как жизнь?!', MAX_VALUE);
