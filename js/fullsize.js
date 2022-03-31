09615bdf351aaddb1daedf7d52d41cd536eca225
sarvanshiriev <sarvan.shiriev@mail.ru>
2022-03-31 18:23:37 +0500

    Изменения от 31.03.2022

--- js/fullsize.js
+++ js/fullsize.js
@@ -1,14 +1,13 @@
-import {randomNumber} from './data.js';
-import {pictureElement} from './data.js';
+// import {randomNumber} from './data.js';
+import {pictureElement} from './miniature.js';
 
 // Удаляем класс "hidden" у элемента ".big-picture"
 const fullSize = document.querySelector('.big-picture');
 
 // Вешаем обработчик на сетку
-pictureElement.addEventListener('click', function() {
-  if (fullSize.classList.remove('hidden')) {
-
-  }
+pictureElement.addEventListener('click', function(event) {
+  fullSize.classList.remove('hidden')
+  console.log(event.target)
 } )
 
 
