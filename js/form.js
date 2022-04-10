eca550bdf94ea88eca7635670beb143ced97a201
sarvanshiriev <sarvan.shiriev@mail.ru>
2022-04-10 15:38:03 +0500

    Изменения от 10.04.22

--- js/form.js
+++ js/form.js
@@ -5,40 +5,44 @@ const uploadForm = document.querySelector('.img-upload__form');
 const pristine = new Pristine(uploadForm);
 // Переменная для пустого состояния поля загрузки "img-upload__label"
 const  uploadLabel = document.querySelector('.img-upload__label');
-
-// Загрузка изображения
+// Переменные загрузки изображения
 const uploadFile = document.getElementById('upload-file');
 const uploadOverlay = document.querySelector('.img-upload__overlay');
+// Переменная для кода для закрытия формы редактирования при клике по иконке закрытия.
+const uploadCancel = document.querySelector('.img-upload__cancel');
+// Переменная для отмены кнопки Esc при фокусе на комментарий
+const textDescription = document.querySelector('.text__description');
+// Хештеги
+const textHashtags = document.querySelector('.text__hashtags');
 
+// Загрузка изображения
 uploadFile.addEventListener('change', () => {
   uploadOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
 });
 
+// Скрытие модального окна и формы редактирования изображения
+const modalClose = function () {
+  uploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  uploadLabel.value='';
+};
 // Код для закрытия формы редактирования при клике по иконке закрытия.
-const uploadCancel = document.querySelector('.img-upload__cancel');
 uploadCancel.addEventListener('click' ,  () => {
   if(!uploadOverlay.classList.contains('hidden')) {
-    uploadOverlay.classList.add('hidden');
-    body.classList.remove('modal-open');
-    uploadLabel.value='';
+    modalClose();
   }
 });
 
 // Код для закрытия формы редактирования по нажатию клавиши Esc
 const onKeyDown = function (evt) {
   if (evt.keyCode === 27 ) {
-    uploadOverlay.classList.add('hidden');
-    body.classList.remove('modal-open');
-    uploadLabel.value='';
-
+    modalClose();
   }
 };
 document.addEventListener('keydown' , onKeyDown);
 
 // Код для отмены кнопки Esc при фокусе на комментарий
-const textDescription = document.querySelector('.text__description');
-
 textDescription.addEventListener('focus', () =>{
   document.removeEventListener('keydown' , onKeyDown);
 });
@@ -46,9 +50,6 @@ textDescription.addEventListener('focus', () =>{
 textDescription.addEventListener('blur', () =>{
   document.addEventListener('keydown' , onKeyDown);
 });
-
-// Хештеги
-const textHashtags = document.querySelector('.text__hashtags');
 
 // Проверка отправки не валидных строк
 uploadForm.addEventListener('submit', (event)=> {
